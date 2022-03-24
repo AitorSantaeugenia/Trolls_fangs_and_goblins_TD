8ab7e79cb32957a36c95c4d6d5835b7108933906
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-03-24 09:02:50 +0100

    added globalCompositeOperation source over so it always show in front of everything, even turrets

--- js/Engine/HPbar.js
+++ js/Engine/HPbar.js
@@ -12,6 +12,7 @@ class HPbar {
 		this.context.fillStyle = this.color;
 		this.img = new Image();
 		this.img.src = 'https://aitorsantaeugenia.github.io/TD_Project1/images/hbar.png';
+		this.context.globalCompositeOperation = 'source-over';
 		this.context.drawImage(this.img, this.posx, this.posy, this.width, this.height);
 	}
 }
