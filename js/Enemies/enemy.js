69e9568a90c7e88411e86469cf09189f2d9aea67
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-01 07:14:42 +0200

    refractor: refractored code

--- js/Enemies/enemy.js
+++ js/Enemies/enemy.js
@@ -1,20 +1,20 @@
 // Class Enemy
 class Enemy {
-	constructor(context, path) {
-		//Setters
-		this.context = context;
-		this.path = path;
-	}
-	//Getters
-	setHP(value) {
-		this.minionHp = value;
-	}
+  constructor(context, path) {
+    //setters
+    this.context = context;
+    this.path = path;
+  }
+  //getters
+  setHP(value) {
+    this.minionHp = value;
+  }
 
-	receiveDamage(value) {
-		this.minionHp -= value;
-	}
+  receiveDamage(value) {
+    this.minionHp -= value;
+  }
 
-	getHP() {
-		return this.minionHp;
-	}
+  getHP() {
+    return this.minionHp;
+  }
 }
