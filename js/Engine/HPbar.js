e7ddce759bc0595362afb5391fa9f31392646d3f
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-03 19:30:21 +0200

    refractor: changed URL to match github pages

--- js/Engine/HPbar.js
+++ js/Engine/HPbar.js
@@ -1,18 +1,25 @@
 class HPbar {
-	constructor(context, posx, posy, width, height, color) {
-		this.context = context;
-		this.posx = posx;
-		this.posy = posy;
-		this.width = width;
-		this.height = height;
-		this.color = color;
-	}
+  constructor(context, posx, posy, width, height, color) {
+    this.context = context;
+    this.posx = posx;
+    this.posy = posy;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
 
-	draw() {
-		this.context.fillStyle = this.color;
-		this.img = new Image();
-		this.img.src = './../../images/utils/hbar.png';
-		this.context.globalCompositeOperation = 'source-over';
-		this.context.drawImage(this.img, this.posx, this.posy, this.width, this.height);
-	}
+  draw() {
+    this.context.fillStyle = this.color;
+    this.img = new Image();
+    this.img.src =
+      "https://aitorsantaeugenia.github.io/Trolls_fangs_and_goblins_TD/images/utils/hbar.png";
+    this.context.globalCompositeOperation = "source-over";
+    this.context.drawImage(
+      this.img,
+      this.posx,
+      this.posy,
+      this.width,
+      this.height
+    );
+  }
 }
