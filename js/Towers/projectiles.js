8ed631ab6e81684a31dfbf3683cf30c31a9632a1
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-03 19:30:52 +0200

    refractor: changed url to match github pages

--- js/Towers/projectiles.js
+++ js/Towers/projectiles.js
@@ -14,10 +14,12 @@ class projectileTurret {
     //load projectiles
     this.img = new Image();
     this.img.type = {
-      sand: "./images/bullets/sandrock.png",
-      catapult: "./images/bullets/rockchain.png",
-      slow: "./images/bullets/snowball.png",
-      flame: "./images/bullets/fireball.png",
+      sand: "https://aitorsantaeugenia.github.io/Trolls_fangs_and_goblins_TD/images/bullets/sandrock.png",
+      catapult:
+        "https://aitorsantaeugenia.github.io/Trolls_fangs_and_goblins_TD/images/bullets/rockchain.png",
+      slow: "https://aitorsantaeugenia.github.io/Trolls_fangs_and_goblins_TD/images/bullets/snowball.png",
+      flame:
+        "https://aitorsantaeugenia.github.io/Trolls_fangs_and_goblins_TD/images/bullets/fireball.png",
     };
     this.img.src = this.img.type[turretType];
     //Width - Height
